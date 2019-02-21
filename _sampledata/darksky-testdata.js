b52b6deb400a5b840617d34bc2178094b0dcc05f
Streusel Cake <streusel.cake@gmail.com>
2019-02-21 10:16:44 +0100

    sampledata

---
+++ _sampledata/darksky-testdata.js
@@ -0,0 +1,9 @@
+var weather = {
+  "latitude": 51.969431,
+  "longitude": 7.595707,
+  "timezone": "Europe/Berlin",
+  "currently": {
+    "time": 1547476756,
+    "summary": "Mostly Cloudy"
+  }
+};
