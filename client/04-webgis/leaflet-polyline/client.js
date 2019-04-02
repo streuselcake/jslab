c721158361454d90d56009ff0cb8281b4176214a
Streusel Cake <streusel.cake@gmail.com>
2019-04-02 23:03:44 +0200

    included base layer again (was excluded for a screenshot)

--- client/04-webgis/leaflet-polyline/client.js
+++ client/04-webgis/leaflet-polyline/client.js
@@ -9,11 +9,11 @@ const start_latlng = [lat, lon];
 
 var map = L.map("mapdiv").setView(start_latlng, 13);
 
-// L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-//   maxZoom: 18,
-//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors",
-//   id: "osm"
-// }).addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 18,
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors",
+  id: "osm"
+}).addTo(map);
 
 
 var gejsonline = gpstracks.features[trackidx].geometry.coordinates;
@@ -21,7 +21,7 @@ var line = [];
 gejsonline.forEach((e, i) => { line.push([gejsonline[i][1],gejsonline[i][0]]); });
 console.log(JSON.stringify(line));
 
-// extracted splitpoints 
+// extracted splitpoints
 var split = [];
 split.push(line[25]);
 split.push(line[45]);
