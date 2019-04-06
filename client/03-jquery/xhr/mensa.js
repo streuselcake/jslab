4547c4ab39887581df7c30a31d9774fc1dcb090a
Streusel Cake <streusel.cake@gmail.com>
2019-04-06 17:42:30 +0200

    few lines added / uncommented

--- client/03-jquery/xhr/mensa.js
+++ client/03-jquery/xhr/mensa.js
@@ -1,18 +1,37 @@
 // jshint esversion: 6
 
-var resource = "https://openmensa.org/api/v2/canteens?near[lat]=51.9606649&near[lng]=7.6261347&near[dist]=15";
-
 // $(document).ready( function() {
 
   // many jquery ajax methods:
   // https://www.w3schools.com/jquery/jquery_ref_ajax.asp
 
-  // call via get or post and ...
-  //$.get( resource );                                          // ... do nothing
+
+  var resource = "https://openmensa.org/api/v2/canteens?near[lat]=51.9606649&near[lng]=7.6261347&near[dist]=15";
+
+  $.ajax({
+    url: resource, // URL der Abfrage,
+    data: {foo: "bar"},
+    type: "GET"
+  })
+  .done (function( response) {
+    $("#content").text(JSON.stringify(response,null,4));
+  })
+  .fail (function( xhr, status, errorThrown ) {
+    // handle errors
+  })
+  .always (function( xhr, status ) {
+    // completed ajax
+  });
+
+
+
+
+  // call via get or post (convenience functions with preset parameters)
+  $.get( resource );                                          // ... do nothing
   //$.get( resource , { p1:"parameter 1", p2:"parameter 2" });  // ... and include parameters in the request
+
   $.get(resource, function(response, status, x){
-    let formatted_response = JSON.stringify(response,null,4);
-    $("#content").text(formatted_response);
+    $("#content").text(JSON.stringify(response,null,4));
     // console.dir(response);
     // console.dir(status);
     // console.dir(x);
