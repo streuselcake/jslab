7b87d0e7cc99c9aa925e060923f85920c69b442e
Streusel Cake <streusel.cake@gmail.com>
2019-04-28 16:10:12 +0200

    node async vs sync / eventloop

---
+++ node/cli/readfile-operation-order.js
@@ -0,0 +1,20 @@
+// jshint esversion: 6
+// jshint node: true
+"use strict";
+
+var fs = require("fs");
+
+console.log("start code");
+
+var data = "";
+fs.readFile("file.txt", "utf-8", (err, data) => {
+  if (!err) {
+    console.log(data);
+  }
+});
+
+process.stdin.on("end",() => {
+  console.log(data);
+});
+
+console.log("end code");
