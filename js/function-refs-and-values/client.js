5f7c7d3fa23dcefd720d36c3188642a1acab3901
Streusel Cake <streusel.cake@gmail.com>
2020-04-09 16:01:40 +0200

    Improved output for new function example

--- js/function-refs-and-values/client.js
+++ js/function-refs-and-values/client.js
@@ -5,33 +5,33 @@ var vInGlobal = "value";
 function changeValue(vInF){
   "use strict";
 
-  console.log("value in function scope before change: " + vInF);
+  console.log("value of vInF in function scope before change: " + vInF);
   vInF="changed value";
-  console.log("value in function scope after change: " + vInF);
+  console.log("value of vInF in function scope after change: " + vInF);
 
 }
 
-console.log("value in global scope before change: " + vInGlobal);
+console.log("value of vInGlobal in global scope before change: " + vInGlobal);
 console.log("calling changeValue...");
 changeValue(vInGlobal);
-console.log("value in global scope after change: " + vInGlobal);
+console.log("value of vInGlobal in global scope after change: " + vInGlobal);
 
 var oInGlobal = {a:"a", b:"b"};
 
 function changeObject(oInF){
   "use strict";
 
-  console.log("object o in function scope before change: ");
+  console.log("object oInF in function scope before change: ");
   console.dir(oInF);
   oInF.a = "c";
-  console.log("object o in function scope after change: ");
+  console.log("object oInF in function scope after change: ");
   console.dir(oInF);
 
 }
 
-console.log("object o in global scope before change: ");
+console.log("object oInGlobal in global scope before change: ");
 console.dir(oInGlobal);
 console.log("calling changeObject...");
 changeObject(oInGlobal);
-console.log("object o in global scope after change: ");
+console.log("object oInGlobal in global scope after change: ");
 console.dir(oInGlobal);
